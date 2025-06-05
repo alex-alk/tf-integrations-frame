5a35e61e396783fec8305da012af201edb9af660
alex-alk <alex.leo06@yahoo.ro>
2025-06-05 18:18:09 +0300

    update

--- support/RequestHandler/FilesystemInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace RequestHandler;
-
-interface FilesystemInterface
-{
-    public function isUploadedFile(string $file): bool;
-    public function moveUploadedFile(string $file, string $target): bool;
-}
